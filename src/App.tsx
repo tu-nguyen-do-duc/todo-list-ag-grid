227188344d69ad0014d534635b6b8a10e820195c
Tu Nguyen <nguyendoductu@gmail.com>
2025-05-06 05:20:22 +0300

    Apply MUI and modify tab menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import "./App.css";
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
 import TodoList from "./TodoList";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
-    <>
+    <Container maxWidth="xl">
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            My Todos
+          </Typography>  
+        </Toolbar>
+      </AppBar>
       <TodoList />
-    </>
+    </Container>
   );
 }
 
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -2,6 +2,14 @@ import { AgGridReact } from "ag-grid-react";
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'; 
 import { useRef, useState } from "react";
 import { ColDef } from "ag-grid-community";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+
 
 // Register all Community features
 ModuleRegistry.registerModules([AllCommunityModule]);
@@ -47,33 +55,39 @@ function TodoList() {
       if (gridRef.current?.api.getSelectedNodes().length) {
         setTodos(
           todos.filter(
-            (todo, index) => index !== Number(gridRef.current?.api.getSelectedNodes()[0].id)
+            (_, index) => index !== Number(gridRef.current?.api.getSelectedNodes()[0].id)
           )
         )
       } else {
         alert("Please select a row to delete first")
       }
     }
-      return (
+
+    return (
         <>
-          <input
-            placeholder="Description"
-            onChange={(e) => setTodo({ ...todo, description: e.target.value })}
-            value={todo.description}
-          />
-          <input
-            placeholder="Priority"
-            onChange={(e) => setTodo({ ...todo, priority: e.target.value })}
-            value={todo.priority}
-          />
-          <input
-            placeholder="Date"
-            type="date"
-            onChange={(e) => setTodo({ ...todo, duedate: e.target.value })}
-            value={todo.duedate}
-          />
-          <button onClick={addTodo}>Add</button>
-          <button onClick={handleDelete}>Delete</button>
+        <Stack mt={2} direction="row" spacing={2} justifyContent="center" alignItems="center">
+            <TextField
+              placeholder="Description"
+              onChange={(e) => setTodo({ ...todo, description: e.target.value })}
+              value={todo.description}
+            />
+            <TextField
+              placeholder="Priority"
+              onChange={(e) => setTodo({ ...todo, priority: e.target.value })}
+              value={todo.priority}
+            />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                    label="Due Date"
+                    value={todo.duedate ? dayjs(todo.duedate) : null}
+                    onChange={(newValue) => {
+                        setTodo({ ...todo, duedate: newValue ? newValue.format('YYYY-MM-DD') : '' })
+                    }}
+                />
+            </LocalizationProvider>
+            <Button variant="contained" onClick={addTodo}>Add</Button>
+            <Button variant="contained" color="error" onClick={handleDelete}>Delete</Button>
+          </Stack>
           <div style={{ width: 700, height: 500 }}>
             <AgGridReact 
               rowData={todos} 
